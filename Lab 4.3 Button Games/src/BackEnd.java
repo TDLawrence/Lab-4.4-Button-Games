bb266b4047f5f563072750ef6651a326a30ef30c
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-03-26 09:19:23 -0400

    Added updateCount method to backEnd

--- Lab 4.3 Button Games/src/BackEnd.java
+++ Lab 4.3 Button Games/src/BackEnd.java
@@ -13,14 +13,21 @@ import java.util.List;
 public class BackEnd {
 
 	private int count;
+	private boolean buttonPressed;
 	
+	public boolean isButtonPressed() {
+		return buttonPressed;
+	}
+
 	public int getCount() {
 		return count;
 	}
 
 	public BackEnd(int x)
 	{this.count=count;
+	 this.buttonPressed=buttonPressed;
 	 count=x;
+	 updateCount(x);
 	 editFile("testcsv.txt",x);	
 	}
     
@@ -29,10 +36,17 @@ public class BackEnd {
 	{Path pathtoFile=Paths.get(filename);
 	 try(BufferedWriter bw=Files.newBufferedWriter(pathtoFile, StandardCharsets.US_ASCII))
 	   { Integer countstring= new Integer(count);
-		 String line=bw.write(countstring.toString()+",");
-		 
+		 bw.write(countstring.toString()+",");
 	   }
 	}
+	
+	public int updateCount(int count)
+	{if (buttonPressed)
+	 {count++;}
+	else
+	 {count=count+0;}
+	}	
+		
 	
 
    }
