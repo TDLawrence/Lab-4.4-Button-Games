f4bc4b5c9210e3b9e77c1dd7b26d67090d07c7a1
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2018-03-27 09:13:07 -0400

    gamecode

--- Lab 4.3 Button Games/src/GameCode.java
+++ Lab 4.3 Button Games/src/GameCode.java
@@ -1,4 +1,33 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class GameCode {
-
+public class GameCode extends Application {
+	
+	public static void main(String[] args) {
+	        launch(args);
+	}
+		public void start(Stage primaryStage) 
+		{   primaryStage.setTitle("Cool");
+	        Button btn = new Button();
+	        btn.setText("Ok");
+	        btn.setOnAction(new EventHandler<ActionEvent>() 
+	             {@Override
+	               public void handle(ActionEvent event) 
+	                  {System.out.println(" clicks");
+	            	  }
+	        	 });
+		
+	            
+	            
+	        StackPane root = new StackPane();
+	        root.getChildren().add(btn);
+	        primaryStage.setScene(new Scene(root, 350, 200));
+	        primaryStage.show();
+		    
+	}
 }
