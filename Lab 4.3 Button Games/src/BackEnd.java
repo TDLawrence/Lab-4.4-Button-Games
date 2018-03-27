5d1982f6fba14d940f1adb984d6976cef76591e7
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-03-27 09:01:51 -0400

    Completed backend code?

--- Lab 4.3 Button Games/src/BackEnd.java
+++ Lab 4.3 Button Games/src/BackEnd.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -32,12 +33,15 @@ public class BackEnd {
 	}
     
 	
-	private static File editFile(String filename,int count)
+	private static void editFile(String filename,int count)
 	{Path pathtoFile=Paths.get(filename);
 	 try(BufferedWriter bw=Files.newBufferedWriter(pathtoFile, StandardCharsets.US_ASCII))
 	   { Integer countstring= new Integer(count);
 		 bw.write(countstring.toString()+",");
 	   }
+	  catch (IOException ioe)
+	 {ioe.printStackTrace();}
+	
 	}
 	
 	public int updateCount(int count)
@@ -45,7 +49,33 @@ public class BackEnd {
 	 {count++;}
 	else
 	 {count=count+0;}
+	return count;
 	}	
+	
+	private static List<String> readScores(String filename)
+	{List<String>Scores=new ArrayList<>();
+	Path pathToFile=Paths.get(filename);
+	try (BufferedReader br=Files.newBufferedReader(pathToFile,StandardCharsets.US_ASCII))
+	 
+	 {String line=br.readLine();
+	  while (line!=null)
+	   {String[]scoreString=line.split(",");
+	    String name=new String(scoreString[0]);
+	    Integer score= new Integer(scoreString[1]);
+	    Scores.add(name+","+score.toString());
+	    line=br.readLine();
+	   }
+	 }
+	  
+	    catch (IOException ioe)
+		{ioe.printStackTrace();
+			
+		}
+	return Scores;
+	}   
+	 
+		
+	
 		
 	
 
@@ -68,4 +98,4 @@ public class BackEnd {
 	
 	
 	
-}
+
